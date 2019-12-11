92745e9ee7610e46cd143e2e94f4ba5b62303fd5
supanee <supanee.som40@gmail.com>
2019-12-11 09:20:59 +0700

    Initial commit

---
+++ src/Lab1/Hello.java
@@ -0,0 +1,17 @@
+package Lab1;
+
+public class Hello {
+
+    public static void  main (String[] args){
+
+        System.out.println("Hello World,I am Supanee");
+        System.out.println("MIT");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("Saiyai");
+        System.out.println("Thungsong");
+        System.out.println("Thailand");
+
+
+
+    }//main
+}//class
